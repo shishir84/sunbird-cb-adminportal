6fa3e101c8de45efbd234c44a0b7321a9a756346
Jenifer Ramsingh <jenifer.ramsingh@tarento.com>
2021-03-10 13:54:56 +0530

    unwanted code removed

--- project/ws/app/src/lib/routes/access/access.module.ts
+++ project/ws/app/src/lib/routes/access/access.module.ts
@@ -1,12 +1,10 @@
 
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { AccessRoutingModule } from './access-routing.module'
 import { BtnPageBackModule, LeftMenuModule, GroupCheckboxModule, UIAdminTableModule } from '@sunbird-cb/collection'
 import { WidgetResolverModule } from '@sunbird-cb/resolver'
 import { HomeModule } from '../home/home.module'
 import { RouterModule } from '@angular/router'
-import { UsersComponent } from './routes/users/users.component'
 import { UsersService } from './services/users.service'
 import {
   MatSidenavModule,
@@ -15,10 +13,8 @@ import {
 import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
-  declarations: [UsersComponent],
-  imports: [CommonModule, AccessRoutingModule, BtnPageBackModule, LeftMenuModule, WidgetResolverModule,
+  imports: [CommonModule, BtnPageBackModule, LeftMenuModule, WidgetResolverModule,
     MatSidenavModule, MatIconModule, GroupCheckboxModule, HomeModule, RouterModule, UIAdminTableModule, MatCardModule],
-  exports: [UsersComponent],
   providers: [UsersService],
 })
 export class AccessModule { }
