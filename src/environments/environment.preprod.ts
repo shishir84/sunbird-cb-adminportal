428f19b7e802cba0e2947c979144d2e498516077
Amit Sengar <aAmitSengar@users.noreply.github.com>
2021-04-26 14:25:21 +0530

    Update environment.preprod.ts

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   sitePath: 'spv-sb.idc.tarento.com',
-  karmYogiPath: 'https://igot-sunbird.idc.tarento.com',
+  karmYogiPath: 'https://igot-pp-sunbird.idc.tarento.com',
 }
